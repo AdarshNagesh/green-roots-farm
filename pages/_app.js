9320edf4882a98f9ffee7579e4b35082a02b1750
AdarshNagesh <31820222+AdarshNagesh@users.noreply.github.com>
2026-04-30 08:01:48 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
+import { useEffect } from 'react'
+import { supabase } from '../lib/supabase'
 import '../styles/globals.css'
 
+const IDLE_TIMEOUT = 5 * 60 * 1000 // 5 minutes
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    let timer
+
+    const reset = () => {
+      clearTimeout(timer)
+      timer = setTimeout(async () => {
+        await supabase.auth.signOut()
+        window.location.href = '/'
+      }, IDLE_TIMEOUT)
+    }
+
+    const events = ['mousemove', 'keypress', 'click', 'touchstart', 'scroll']
+    events.forEach(e => window.addEventListener(e, reset))
+    reset()
+
+    return () => {
+      clearTimeout(timer)
+      events.forEach(e => window.removeEventListener(e, reset))
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }

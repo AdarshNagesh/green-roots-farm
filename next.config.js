9a3f4ef76eec4e92e8f380d7bf338180197e779e
AdarshNagesh <31820222+AdarshNagesh@users.noreply.github.com>
2026-04-30 16:04:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withSentryConfig(nextConfig, {
+  silent: true,         // suppress CLI output
+  org:    'adarshinifarm',   // your Sentry org slug
+  project:'javascript-nextjs', // your Sentry project slug
+}, {
+  widenClientFileUpload: true,
+  hideSourceMaps: true, // don't expose source maps to browser
+  disableLogger: true,
+})
